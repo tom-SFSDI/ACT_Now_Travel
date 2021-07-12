f00353f6237cff8f5f7e3b8de98763736deb4272
Tom-SFSDI <tlendy25@yahoo.com>
2021-07-12 16:29:08 -0500

    adding some basic js function to conv Celc to Far.

--- src/script/index.js
+++ src/script/index.js
@@ -1,4 +1,7 @@
-const someJSToStart = () => {
-  alert("hello")
+document.getElementById('destination_search').addEventListener('click', () => {
+  document.location.href = '../html/wiki.html';
+})
+
+function convertCelcToFar(tempInCelc) {
+  return Math.floor(tempInCelc * 1.8 + 32)
 }
-someJSToStart()
