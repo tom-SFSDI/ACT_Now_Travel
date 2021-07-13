6fb0f168f69dbca1fb7d80f951f97ccfc00e95d2
Holman-StateFarm <chris.holman.mkjw@statefarm.com>
2021-07-13 10:21:11 -0500

    condensed html and made first api fetch

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,23 @@
-document.getElementById('destination_search').addEventListener('click', () => {
-  document.location.href = '../html/wiki.html';
+document.getElementById('get_info').addEventListener('click', () => {
+
+  var value = document.getElementById('destination_dropdown').value
+  console.log(value)
+
+  fetch('https://goweather.herokuapp.com/weather/' + value)
+    .then(function(response) {
+      console.log("you made it")
+      return response.json()
+    })
+    .then(function(data) {
+      console.log(data)
+  })
+
+
+
+
+  //document.location.href = '../html/confirmation.html';
 })
 
-function convertCelcToFar(tempInCelc) {
-  return Math.floor(tempInCelc * 1.8 + 32)
-}
+// function convertCelcToFar(tempInCelc) {
+//   return Math.floor(tempInCelc * 1.8 + 32)
+// }
