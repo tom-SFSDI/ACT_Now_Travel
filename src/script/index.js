9813e90f6037920f356bf117df5ddb4452b9149a
Tom-SFSDI <tlendy25@yahoo.com>
2021-07-13 12:16:55 -0500

    added fetch skywatch API for lowest flight prices

--- src/script/index.js
+++ src/script/index.js
@@ -2,7 +2,20 @@ document.getElementById('get_info').addEventListener('click', () => {
 
   var value = document.getElementById('destination_dropdown').value
   let weatherForecast = {};
-
+  const airportCodes = [`DFW-sky/`, `MCO-sky/`, `LAX-sky/`, `MSY-sky/`, `JFK-sky/`]
+  const airportSelected = (string) => {
+    if (string === `Dallas`) {
+      return airportCodes[0]
+    } else if (string === `Orlando`) {
+      return airportCodes[1]
+    } else if (string === `LosAngeles`) {
+      return airportCodes[2]
+    } else if (string === `NewOrleans`) {
+      return airportCodes[3]
+    } else {
+      return airportCodes[4]
+    }
+  }
 
   fetch('https://goweather.herokuapp.com/weather/' + value)
     .then(function(response) {
@@ -18,15 +31,31 @@ document.getElementById('get_info').addEventListener('click', () => {
       document.getElementById('day3Temp').innerText = weatherForecast.forecast[2].temperature;
 
       document.getElementById('todaysWeather').innerText = weatherForecast.description;
-      
-
-  })
-
-
-
-
+    })
+  let baseURL = `https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/ORD-sky/`
+    // console.log(airportSelected(value))
+    // document.getElementById('destination_dropdown').value()
+  fetch(`${baseURL}${airportSelected(value)}2021-08-15?inboundpartialdate=2021-09-12`, {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-key": "e8336ed4dbmshc74bad6810bd5a9p1ea62djsne419c89fee3b",
+        "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+      }
+    })
+    .then(response => {
+      // console.log(response.json());
+      return response.json()
+    })
+    .then(data => {
+      console.log(data)
+      data.Quotes[0].MinPrice
+    })
+    .catch(err => {
+      console.error(err);
+    });
   //document.location.href = '../html/confirmation.html';
 })
+
 
 function convertCelcToFar(tempInCelc) {
   console.log(tempInCelc);
