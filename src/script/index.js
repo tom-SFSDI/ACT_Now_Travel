c68780e7bdb6f2287cfdd9348059bb90cb141137
ALang-c7dg <anne.lang.c7dg@statefarm.com>
2021-07-13 11:12:05 -0500

    pulled API data into table

--- src/script/index.js
+++ src/script/index.js
@@ -1,7 +1,8 @@
 document.getElementById('get_info').addEventListener('click', () => {
 
   var value = document.getElementById('destination_dropdown').value
-  console.log(value)
+  let weatherForecast = {};
+
 
   fetch('https://goweather.herokuapp.com/weather/' + value)
     .then(function(response) {
@@ -10,6 +11,15 @@ document.getElementById('get_info').addEventListener('click', () => {
     })
     .then(function(data) {
       console.log(data)
+      weatherForecast = data;
+      // let convertedTemp1 = convertCelcToFar(weatherForecast.forecast[0].temperature);
+      document.getElementById('day1Temp').innerText = weatherForecast.forecast[0].temperature;
+      document.getElementById('day2Temp').innerText = weatherForecast.forecast[1].temperature;
+      document.getElementById('day3Temp').innerText = weatherForecast.forecast[2].temperature;
+
+      document.getElementById('todaysWeather').innerText = weatherForecast.description;
+      
+
   })
 
 
@@ -18,6 +28,7 @@ document.getElementById('get_info').addEventListener('click', () => {
   //document.location.href = '../html/confirmation.html';
 })
 
-// function convertCelcToFar(tempInCelc) {
-//   return Math.floor(tempInCelc * 1.8 + 32)
-// }
+function convertCelcToFar(tempInCelc) {
+  console.log(tempInCelc);
+  return Math.floor(tempInCelc * 1.8 + 32)
+}
