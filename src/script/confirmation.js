bf2b69ac1489583b6a609cb550f8e2e20572b14e
Tom-SFSDI <tlendy25@yahoo.com>
2021-07-14 11:27:05 -0500

    additional logic to populate things on the receipt

--- src/script/confirmation.js
+++ src/script/confirmation.js
@@ -1,3 +1,11 @@
-document.getElementById('paidButton').onclick = function(){
-    alert("Thank you for your identity!  And bitcoins!")
- }
+document.getElementById('paidButton').onclick = function() {
+  const customer = document.getElementById('name').value
+  const address = document.getElementById('street').value
+  const email = document.getElementById('email').value
+  document.querySelector('.invoiceBox').style.display = 'block'
+    //   document.getElementById('customer').innerText = customer
+    //   document.getElementById('customer').append(`${address}`)
+  document.getElementById('customer').append(customer, document.createElement('br'), address, document.createElement('br'), email)
+
+  alert("How could you possibly have trusted this janky website! We now have your identity and bitcoins!")
+}
